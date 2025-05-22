1da19025b652b03cb7dc4c8e08ac21a1ca978836
Twafique <myotubesh0rt@gmail.com>
2025-05-22 17:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,6 @@ def transcribe():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+    @app.route('/healthz')
+def health_check():
+    return "OK", 200
